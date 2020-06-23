18ec5160581e6216229c63043e51a79d119e5f0e
hisnameispum <sirasitxp@gmail.com>
2020-06-23 02:44:56 -0700

    Added randomization, Made UIimageviews updated when button is clicked.

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,10 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var imageDice1: UIImageView!
+    @IBOutlet weak var imageDice2: UIImageView!
+    let diceImage = [ #imageLiteral(resourceName: "DiceOne"),#imageLiteral(resourceName: "DiceTwo"),#imageLiteral(resourceName: "DiceThree"),#imageLiteral(resourceName: "DiceFour"),#imageLiteral(resourceName: "DiceFive"),#imageLiteral(resourceName: "DiceSix")]
+    var index1 = 0
+    var index2 = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func buttonRoll(_ sender: UIButton) {
+        index1 = Int.random(in: 0 ... 5)
+        index2 = Int.random(in: 0 ... 5)
+        imageDice1.image = diceImage[index1]
+        imageDice2.image = diceImage[index2]
     }
 
 
